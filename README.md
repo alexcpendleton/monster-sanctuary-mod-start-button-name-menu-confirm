92206226084330e4bbf3c542eac49a41a7ad1b99
Alexander Pendleton <alexcpendleton@gmail.com>
2021-12-20 16:57:03 -0800

    Initial commit, and it works :)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# monster-sanctuary-mod-start-button-name-menu-confirm
- Pressing "Start" on your controller selects the "Confirm" button (similar to how Switch keyboards often do)
+# monster-sanctuary-mod-monster-checklist
---
+++ owl7829wg.StartButtonNameMenuConfirm/StartButtonNameMenuConfirmPlugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using MonoMod;
+using UnityEngine;
+
+namespace owl7829wg.StartButtonNameMenuConfirm
+{
+    // You use BepInDependency to mark a mod that your mod requires to work. This is not required if you don't have dependencies.
+    [BepInDependency("evaisa.MonSancAPI")]
+    // BepInPlugin is required to make BepInEx properly load your mod, this tells BepInEx the ID, Name and Version of your mod.
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class StartButtonNameMenuConfirmPlugin : BaseUnityPlugin
+    {
+        public const string ModGUID = "owl7829wg.StartButtonNameMenuConfirmPlugin";
+        public const string ModName = "StartButtonNameMenuConfirmPlugin";
+        public const string ModVersion = "1.0.0";
+
+        // This is the first code that runs when your mod gets loaded.
+        public StartButtonNameMenuConfirmPlugin()
+        {
+            On.NameMenu.Update += NameMenu_Update;
+        }
+
+        private void NameMenu_Update(On.NameMenu.orig_Update orig, NameMenu self)
+        {
+            var startButtons = new[]
+            {
+                KeyCode.JoystickButton7             
+            };
+
+            foreach(var button in startButtons)
+            {
+                if(Input.GetKeyUp(button))
+                {
+                    Logger.LogInfo($"Pressed {button}");
+                    self.MenuList.SelectMenuItem(self.ConfirmKey);
+                }
+            }
+            orig(self);
+        }
+    }
+}
